318985470e49d261e063ab614eb2013f66227501
agorobets <agorobets@mesosphere.io>
2019-07-26 11:47:41 +0200

    [DCOS-56116] Install Kibana plugin according searchguard docs

--- frameworks/elastic/kibana/init.sh
+++ frameworks/elastic/kibana/init.sh
@@ -25,16 +25,6 @@ if [ -n  "${CUSTOM_YAML_BLOCK_BASE64}" ]; then
   CUSTOM_YAML_BLOCK=$(echo "${CUSTOM_YAML_BLOCK_BASE64}" | base64 -d)
 fi
 
-if [ "$SEARCHGUARD_ENABLED" == true ]; then
-  SEARCHGUARD_YAML_BLOCK="searchguard.basicauth.enabled: true"
-
-  if [ -n "$KIBANA_PLUGINS" ]; then
-    KIBANA_PLUGINS="$KIBANA_PLUGINS$IFS$SEARCHGUARD_PLUGIN_URI"
-  else
-    KIBANA_PLUGINS="$SEARCHGUARD_PLUGIN_URI"
-  fi
-fi
-
 cat <<-EOF > "${KIBANA_YML_PATH}"
 	elasticsearch.url: "${ELASTICSEARCH_URL}"
 	elasticsearch.username: "${KIBANA_USER}"
@@ -50,23 +40,37 @@ cat <<-EOF > "${KIBANA_YML_PATH}"
 	xpack.security.encryptionKey: "${MESOS_FRAMEWORK_ID}"
 	xpack.reporting.encryptionKey: "${MESOS_FRAMEWORK_ID}"
 
-	${SEARCHGUARD_YAML_BLOCK}
 	${CUSTOM_YAML_BLOCK}
 EOF
-
-if [ -n "$KIBANA_PLUGINS" ]; then 
-  echo 'Installing Kibana plugins.'
-  read -ra PLUGINS <<< "$KIBANA_PLUGINS"
-  for plugin in "${PLUGINS[@]}"; do
-    echo "Installing plugin: ${plugin}" 
-    ${KIBANA_PATH}/bin/kibana-plugin install "$plugin"
-  done; 
-fi
 
 if [ "${KIBANA_ELASTICSEARCH_TLS}" = "true" ]; then
   cat <<-EOF >> "${KIBANA_YML_PATH}"
 		elasticsearch.ssl.certificateAuthorities: ${MESOS_SANDBOX}/.ssl/ca-bundle.crt
 	EOF
+fi
+
+if [ "${SEARCHGUARD_ENABLED}" = "true" ]; then
+  NODE_OPTIONS="--max-old-space-size=8192"
+
+  cat <<-EOF >> "${KIBANA_YML_PATH}"
+		searchguard.basicauth.enabled: true
+		elasticsearch.hosts: "${ELASTICSEARCH_URL}"
+	EOF
+
+  if [ -n "$KIBANA_PLUGINS" ]; then
+    KIBANA_PLUGINS="$KIBANA_PLUGINS$IFS$SEARCHGUARD_PLUGIN_URI"
+  else
+    KIBANA_PLUGINS="$SEARCHGUARD_PLUGIN_URI"
+  fi
+fi
+
+if [ -n "$KIBANA_PLUGINS" ]; then
+  echo 'Installing Kibana plugins.'
+  read -ra PLUGINS <<< "$KIBANA_PLUGINS"
+  for plugin in "${PLUGINS[@]}"; do
+    echo "Installing plugin: ${plugin}"
+    ${KIBANA_PATH}/bin/kibana-plugin install "$plugin"
+  done;
 fi
 
 exec "${MESOS_SANDBOX}/kibana-$ELASTIC_VERSION-linux-x86_64/bin/kibana"
