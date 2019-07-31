846f934a0d8df202d6f069888402596313350ab6
agorobets <agorobets@mesosphere.io>
2019-07-31 12:02:23 +0200

    [DCOS-56116] Black formatting

--- frameworks/elastic/tests/test_searchguard.py
+++ frameworks/elastic/tests/test_searchguard.py
@@ -1,4 +1,3 @@
-
 from typing import Any, Dict, Iterator
 
 import pytest
@@ -11,27 +10,29 @@ import sdk_utils
 from tests import config
 from security import transport_encryption
 
-sg_internal_users_secret_name="sg_internal_users"
+sg_internal_users_secret_name = "sg_internal_users"
+
 
 def _uninstall_services() -> None:
     sdk_install.uninstall(config.PACKAGE_NAME, config.SERVICE_NAME)
+
 
 @pytest.fixture
 def sg_internal_users_path(tmp_path) -> str:
     path = tmp_path / "sg_internal_users.yml"
     sg_internal_users_config = (
-        'admin.readonly: true\n'
-        'admin.hash: "$2y$12$Pd3kIQD1WgaKpekPyMkUi.jmBDF3QDmPIEUg37wXCRufZZQOnYYYW"\n' # password: admin_password
-        'admin.roles: [ admin ]\n'
-        
-        'kibanaserver.readonly: true\n'
-        'kibanaserver.hash: "$2y$12$lrwJYnjrlTGgOcf7Kd6xXOHIbkqFWBO3qgqyLYlEwIuRM3CfgL5fG"\n' # password: kibanaserver_password
-
-        'kibanaro.hash: "$2y$12$gaYwLqeOWzvWXuvf5OnpFe3kqsqKggxHiiMp/JG9hq2nnuxTFtcN."\n' # password: kibanaro_password
-        'kibanaro.roles: [kibanauser, readall]\n')
+        "admin.readonly: true\n"
+        'admin.hash: "$2y$12$Pd3kIQD1WgaKpekPyMkUi.jmBDF3QDmPIEUg37wXCRufZZQOnYYYW"\n'  # password: admin_password
+        "admin.roles: [ admin ]\n"
+        "kibanaserver.readonly: true\n"
+        'kibanaserver.hash: "$2y$12$lrwJYnjrlTGgOcf7Kd6xXOHIbkqFWBO3qgqyLYlEwIuRM3CfgL5fG"\n'  # password: kibanaserver_password
+        'kibanaro.hash: "$2y$12$gaYwLqeOWzvWXuvf5OnpFe3kqsqKggxHiiMp/JG9hq2nnuxTFtcN."\n'  # password: kibanaro_password
+        "kibanaro.roles: [kibanauser, readall]\n"
+    )
 
     path.write_text(sg_internal_users_config)
     return path.resolve()
+
 
 @pytest.fixture(scope="module")
 def service_account(configure_security: None) -> Iterator[Dict[str, Any]]:
@@ -42,11 +43,16 @@ def service_account(configure_security: None) -> Iterator[Dict[str, Any]]:
     finally:
         transport_encryption.cleanup_service_account(config.SERVICE_NAME, service_account_info)
 
+
 @pytest.mark.sanity
 def test_searchguard_support_elastic(service_account: Dict[str, Any], sg_internal_users_path: str):
     try:
         _uninstall_services()
-        sdk_cmd.run_cli('security secrets create -f {} {}'.format(str(sg_internal_users_path), sg_internal_users_secret_name))
+        sdk_cmd.run_cli(
+            "security secrets create -f {} {}".format(
+                str(sg_internal_users_path), sg_internal_users_secret_name
+            )
+        )
 
         sdk_install.install(
             config.PACKAGE_NAME,
@@ -59,13 +65,22 @@ def test_searchguard_support_elastic(service_account: Dict[str, Any], sg_interna
                     "service_account_secret": service_account["secret"],
                     "security": {"transport_encryption": {"enabled": True}},
                 },
-                "sgadmin_nodes" : {"count" : 1},
-                "elasticsearch": {"searchguard_enabled": True, "health_user": "admin", "health_user_password": "admin_password", "searchguard_internal_users" : sg_internal_users_secret_name},
+                "sgadmin_nodes": {"count": 1},
+                "elasticsearch": {
+                    "searchguard_enabled": True,
+                    "health_user": "admin",
+                    "health_user_password": "admin_password",
+                    "searchguard_internal_users": sg_internal_users_secret_name,
+                },
             },
         )
 
-        curl_cmd = 'curl -L -i -k -s {}/_searchguard/health'.format("https://" + sdk_hosts.vip_host(config.SERVICE_NAME, "coordinator", 9200))
-        rc, stdout, stderr = sdk_cmd.service_task_exec(config.SERVICE_NAME, "master-0-node", curl_cmd)
+        curl_cmd = "curl -L -i -k -s {}/_searchguard/health".format(
+            "https://" + sdk_hosts.vip_host(config.SERVICE_NAME, "coordinator", 9200)
+        )
+        rc, stdout, stderr = sdk_cmd.service_task_exec(
+            config.SERVICE_NAME, "master-0-node", curl_cmd
+        )
         assert bool(rc == 0 and stdout and "HTTP/1.1 200" in stdout and '"status":"UP"' in stdout)
 
     finally:
@@ -77,7 +92,11 @@ def test_searchguard_support_elastic(service_account: Dict[str, Any], sg_interna
 def test_searchguard_support_kibana(service_account: Dict[str, Any], sg_internal_users_path: str):
     try:
         _uninstall_services()
-        sdk_cmd.run_cli('security secrets create -f {} {}'.format(str(sg_internal_users_path), sg_internal_users_secret_name))
+        sdk_cmd.run_cli(
+            "security secrets create -f {} {}".format(
+                str(sg_internal_users_path), sg_internal_users_secret_name
+            )
+        )
 
         sdk_install.install(
             config.PACKAGE_NAME,
@@ -90,26 +109,42 @@ def test_searchguard_support_kibana(service_account: Dict[str, Any], sg_internal
                     "service_account_secret": service_account["secret"],
                     "security": {"transport_encryption": {"enabled": True}},
                 },
-                "sgadmin_nodes" : {"count" : 1},
-                "elasticsearch": {"searchguard_enabled": True, "health_user": "admin", "health_user_password": "admin_password", "searchguard_internal_users" : sg_internal_users_secret_name},
+                "sgadmin_nodes": {"count": 1},
+                "elasticsearch": {
+                    "searchguard_enabled": True,
+                    "health_user": "admin",
+                    "health_user_password": "admin_password",
+                    "searchguard_internal_users": sg_internal_users_secret_name,
+                },
             },
         )
 
-        elasticsearch_url = "https://" + sdk_hosts.vip_host(config.SERVICE_NAME, "coordinator", 9200)
+        elasticsearch_url = "https://" + sdk_hosts.vip_host(
+            config.SERVICE_NAME, "coordinator", 9200
+        )
 
         sdk_install.install(
             config.KIBANA_PACKAGE_NAME,
             config.KIBANA_SERVICE_NAME,
             0,
             {
-              "kibana": {"elasticsearch_url": elasticsearch_url, "elasticsearch_tls": True, "searchguard_enabled": True, "user": "kibanaserver", "password": "kibanaserver_password"},
+                "kibana": {
+                    "elasticsearch_url": elasticsearch_url,
+                    "elasticsearch_tls": True,
+                    "searchguard_enabled": True,
+                    "user": "kibanaserver",
+                    "password": "kibanaserver_password",
+                }
             },
             wait_for_deployment=False,
             insert_strict_options=False,
         )
 
-        curl_cmd = 'curl -L -i -k -u kibanaserver:kibanaserver_password -s {}/{}'.format(
-            sdk_utils.dcos_url().rstrip("/"), "service/{}/{}".format(config.KIBANA_PACKAGE_NAME, "api/saved_objects/_find?type=index-pattern").lstrip("/")
+        curl_cmd = "curl -L -i -k -u kibanaserver:kibanaserver_password -s {}/{}".format(
+            sdk_utils.dcos_url().rstrip("/"),
+            "service/{}/{}".format(
+                config.KIBANA_PACKAGE_NAME, "api/saved_objects/_find?type=index-pattern"
+            ).lstrip("/"),
         )
         rc, stdout, _ = sdk_cmd.master_ssh(curl_cmd)
         assert bool(rc == 0 and stdout and "HTTP/1.1 200" in stdout and '"status":"UP"' in stdout)
