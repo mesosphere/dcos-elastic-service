3326fd507ea2a43d7723227c1c6e3a0c90d13a7a
agorobets <agorobets@mesosphere.io>
2019-08-05 14:11:31 +0200

    [DCOS-56116] reuse Elastic with searchguard code in test

--- frameworks/elastic/tests/test_searchguard.py
+++ frameworks/elastic/tests/test_searchguard.py
@@ -7,14 +7,12 @@ import sdk_hosts
 import sdk_security
 
 from tests import config
-from security import transport_encryption
+from tests.commons import tls
 
 sg_internal_users_secret_name = "sg_internal_users"
 
-
 def _uninstall_services() -> None:
     sdk_install.uninstall(config.PACKAGE_NAME, config.SERVICE_NAME)
-
 
 @pytest.fixture
 def sg_internal_users_path(tmp_path) -> str:
@@ -32,19 +30,12 @@ def sg_internal_users_path(tmp_path) -> str:
     path.write_text(sg_internal_users_config)
     return path.resolve()
 
-
 @pytest.fixture(scope="module")
 def service_account(configure_security: None) -> Iterator[Dict[str, Any]]:
-    service_account_info = transport_encryption.setup_service_account(config.SERVICE_NAME)
-    try:
+    yield from tls._service_account_impl(configure_security)
 
-        yield service_account_info
-    finally:
-        transport_encryption.cleanup_service_account(config.SERVICE_NAME, service_account_info)
-
-
-@pytest.mark.sanity
-def test_searchguard_support_elastic(service_account: Dict[str, Any], sg_internal_users_path: str):
+@pytest.fixture
+def elastic_searchguard(service_account: Dict[str, Any], sg_internal_users_path: str):
     try:
         _uninstall_services()
         sdk_cmd.run_cli(
@@ -53,7 +44,7 @@ def test_searchguard_support_elastic(service_account: Dict[str, Any], sg_interna
             )
         )
 
-        sdk_install.install(
+        yield sdk_install.install(
             config.PACKAGE_NAME,
             service_name=config.SERVICE_NAME,
             expected_running_tasks=config.DEFAULT_TASK_COUNT,
@@ -73,50 +64,27 @@ def test_searchguard_support_elastic(service_account: Dict[str, Any], sg_interna
                 },
             },
         )
-
-        curl_cmd = "curl -L -i -k -s {}/_searchguard/health".format(
-            "https://" + sdk_hosts.vip_host(config.SERVICE_NAME, "coordinator", 9200)
-        )
-        rc, stdout, stderr = sdk_cmd.service_task_exec(
-            config.SERVICE_NAME, "master-0-node", curl_cmd
-        )
-        assert bool(rc == 0 and stdout and "HTTP/1.1 200" in stdout and '"status":"UP"' in stdout)
-
     finally:
         _uninstall_services()
         sdk_security.delete_secret(sg_internal_users_secret_name)
 
 
-@pytest.mark.sanity
-def test_searchguard_support_kibana(service_account: Dict[str, Any], sg_internal_users_path: str):
-    try:
-        _uninstall_services()
-        sdk_cmd.run_cli(
-            "security secrets create -f {} {}".format(
-                str(sg_internal_users_path), sg_internal_users_secret_name
-            )
-        )
 
-        sdk_install.install(
-            config.PACKAGE_NAME,
-            service_name=config.SERVICE_NAME,
-            expected_running_tasks=config.DEFAULT_TASK_COUNT,
-            additional_options={
-                "service": {
-                    "name": config.SERVICE_NAME,
-                    "service_account": service_account["name"],
-                    "service_account_secret": service_account["secret"],
-                    "security": {"transport_encryption": {"enabled": True}},
-                },
-                "sgadmin_nodes": {"count": 1},
-                "elasticsearch": {
-                    "searchguard_enabled": True,
-                    "health_user": "admin",
-                    "health_user_password": "admin_password",
-                    "searchguard_internal_users": sg_internal_users_secret_name,
-                },
-            },
-        )
+@pytest.mark.sanity
+def test_searchguard_support_elastic(elastic_searchguard):
+    curl_cmd = "curl -L -i -k -s {}/_searchguard/health".format(
+        "https://" + sdk_hosts.vip_host(config.SERVICE_NAME, "coordinator", 9200)
+    )
+    rc, stdout, stderr = sdk_cmd.service_task_exec(
+        config.SERVICE_NAME, "master-0-node", curl_cmd
+    )
+    assert bool(rc == 0 and stdout and "HTTP/1.1 200" in stdout and '"status":"UP"' in stdout)
+
+
+@pytest.mark.sanity
+def test_searchguard_support_kibana(elastic_searchguard):
+    try:
+        sdk_install.uninstall(config.KIBANA_PACKAGE_NAME, config.KIBANA_SERVICE_NAME)
 
         elasticsearch_url = "https://" + sdk_hosts.vip_host(
             config.SERVICE_NAME, "coordinator", 9200
@@ -148,5 +116,3 @@ def test_searchguard_support_kibana(service_account: Dict[str, Any], sg_internal
 
     finally:
         sdk_install.uninstall(config.KIBANA_PACKAGE_NAME, config.KIBANA_SERVICE_NAME)
-        _uninstall_services()
-        sdk_security.delete_secret(sg_internal_users_secret_name)
