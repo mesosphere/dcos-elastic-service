22e1bdd84adff3a8d754db66d929c4d8bc0dfc08
ishanchandrajoshi <ishan.joshi2@tcs.com>
2019-11-13 17:42:55 +0530

    added test for backup and restore and a check for affirming that data is deleted before restore

---
+++ frameworks/elastic/tests/test_backup_restore.py
@@ -0,0 +1,139 @@
+import pytest
+import os
+from typing import Iterator
+import sdk_install
+import sdk_hosts
+import sdk_cmd
+import sdk_tasks
+from tests import config
+
+
+@pytest.fixture(scope="module", autouse=True)
+def configure_package(configure_security: None) -> Iterator[None]:
+
+    try:
+        service_options = {"elasticsearch": {"plugins": "repository-s3"}}
+        sdk_install.uninstall(config.PACKAGE_NAME, config.SERVICE_NAME)
+        sdk_install.install(
+            config.PACKAGE_NAME,
+            config.SERVICE_NAME,
+            config.DEFAULT_TASK_COUNT,
+            additional_options=service_options,
+        )
+
+        yield  # let the test session execute
+    finally:
+        sdk_install.uninstall(config.PACKAGE_NAME, config.SERVICE_NAME)
+
+
+@pytest.fixture(autouse=True)
+def pre_test_setup() -> None:
+    sdk_tasks.check_running(config.SERVICE_NAME, config.DEFAULT_TASK_COUNT)
+    config.wait_for_expected_nodes_to_exist(task_count=config.DEFAULT_TASK_COUNT)
+
+
+@pytest.mark.aws
+@pytest.mark.sanity
+def test_backup_restore():
+    key_id = os.getenv("AWS_ACCESS_KEY_ID")
+    if not key_id:
+        assert (
+            False
+        ), 'AWS credentials are required for this test. Disable test with e.g. TEST_TYPES="sanity and not aws"'
+    task_list = sdk_tasks.get_service_tasks(config.SERVICE_NAME)
+    for task in task_list:
+        sdk_cmd.service_task_exec(
+            config.SERVICE_NAME,
+            task.name,
+            "bash -c \"export JAVA_HOME=$(ls -d $MESOS_SANDBOX/jdk*/);  echo '{}' | ./elasticsearch-*/bin/elasticsearch-keystore add --stdin s3.client.default.access_key\"".format(
+                os.getenv("AWS_ACCESS_KEY_ID")
+            ),
+        )
+        sdk_cmd.service_task_exec(
+            config.SERVICE_NAME,
+            task.name,
+            "bash -c \"export JAVA_HOME=$(ls -d $MESOS_SANDBOX/jdk*/);  echo '{}' | ./elasticsearch-*/bin/elasticsearch-keystore add --stdin s3.client.default.secret_key\"".format(
+                os.getenv("AWS_SECRET_ACCESS_KEY")
+            ),
+        )
+        sdk_cmd.service_task_exec(
+            config.SERVICE_NAME,
+            task.name,
+            "bash -c \"export JAVA_HOME=$(ls -d $MESOS_SANDBOX/jdk*/);  echo '{}' | ./elasticsearch-*/bin/elasticsearch-keystore add --stdin s3.client.default.session_token\"".format(
+                os.getenv("AWS_SESSION_TOKEN")
+            ),
+        )
+        sdk_cmd.service_task_exec(
+            config.SERVICE_NAME,
+            task.name,
+            'bash -c "export JAVA_HOME=$(ls -d $MESOS_SANDBOX/jdk*/); ./elasticsearch-*/bin/elasticsearch-keystore list"',
+        )
+
+    sdk_cmd.service_task_exec(
+        config.SERVICE_NAME,
+        "master-0-node",
+        " /opt/mesosphere/bin/curl -i -XPOST -H 'Content-type: application/json' \"http://"
+        + sdk_hosts.vip_host(config.SERVICE_NAME, "coordinator", 9200)
+        + '/_nodes/reload_secure_settings"',
+    )
+    sdk_cmd.service_task_exec(
+        config.SERVICE_NAME,
+        "master-0-node",
+        '  /opt/mesosphere/bin/curl -i -XPOST -H \'Content-type: application/json\' -d \'{"name": "SwamiVivekananda"}\' "http://'
+        + sdk_hosts.vip_host(config.SERVICE_NAME, "coordinator", 9200)
+        + '/customer/entry/99?pretty"',
+    )
+    sdk_cmd.service_task_exec(
+        config.SERVICE_NAME,
+        "master-0-node",
+        ' /opt/mesosphere/bin/curl -i -XPUT -H \'Content-type: application/json\' -d \'{"type": "s3", "settings": {"bucket": "elastic-bkp-bucket", "region": "us-east-1"} }\' "http://'
+        + sdk_hosts.vip_host(config.SERVICE_NAME, "coordinator", 9200)
+        + '/_snapshot/s3_repo?verify=false&pretty"',
+    )
+
+    # take backup
+    sdk_cmd.service_task_exec(
+        config.SERVICE_NAME,
+        "master-0-node",
+        " /opt/mesosphere/bin/curl -i -XPUT -H 'Content-type: application/json' \"http://"
+        + sdk_hosts.vip_host(config.SERVICE_NAME, "coordinator", 9200)
+        + '/_snapshot/s3_repo/snap1?"',
+    )
+
+    # Delete data before executing restore
+    sdk_cmd.service_task_exec(
+        config.SERVICE_NAME,
+        "master-0-node",
+        " /opt/mesosphere/bin/curl -i -XDELETE -H 'Content-type: application/json' \"http://"
+        + sdk_hosts.vip_host(config.SERVICE_NAME, "coordinator", 9200)
+        + '/*"',
+    ) 
+
+    _, output, _ = sdk_cmd.service_task_exec(
+        config.SERVICE_NAME,
+        "master-0-node",
+        " /opt/mesosphere/bin/curl -i -u elastic:changeme -H 'Content-type: application/json' \"http://"
+        + sdk_hosts.vip_host(config.SERVICE_NAME, "coordinator", 9200)
+        + '/customer/entry/99?pretty"',
+    )
+
+    assert '"name" : "SwamiVivekananda"' not in output
+
+    # restore data
+    sdk_cmd.service_task_exec(
+        config.SERVICE_NAME,
+        "master-0-node",
+        " /opt/mesosphere/bin/curl -i -XPOST -H 'Content-type: application/json' \"http://"
+        + sdk_hosts.vip_host(config.SERVICE_NAME, "coordinator", 9200)
+        + '/_snapshot/s3_repo/snap1/_restore"',
+    )
+
+    _, output, _ = sdk_cmd.service_task_exec(
+        config.SERVICE_NAME,
+        "master-0-node",
+        " /opt/mesosphere/bin/curl -i -u elastic:changeme -H 'Content-type: application/json' \"http://"
+        + sdk_hosts.vip_host(config.SERVICE_NAME, "coordinator", 9200)
+        + '/customer/entry/99?pretty"',
+    )
+
+    assert '"name" : "SwamiVivekananda"' in output
