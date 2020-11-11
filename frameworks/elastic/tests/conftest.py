b6611700dc78cca23cd290761b0303f8e1c409c2
agorobets <agorobets@mesosphere.io>
2020-11-11 15:38:17 +0100

    add ENABLE_EXTERNAL_VOLUMES variable check

--- frameworks/elastic/tests/conftest.py
+++ frameworks/elastic/tests/conftest.py
@@ -1,5 +1,6 @@
 from typing import Iterator
 
+import os
 import pytest
 import sdk_security
 import sdk_external_volumes
@@ -39,5 +40,10 @@ def pytest_runtest_setup(item):
 
 @pytest.fixture(scope="session")
 def configure_external_volumes():
-    # Handle creation of external volumes.
-    yield from sdk_external_volumes.external_volumes_session()
+    if is_env_var_set("ENABLE_EXTERNAL_VOLUMES", default=str(False)):
+        yield from sdk_external_volumes.external_volumes_session()
+    else:
+        yield
+
+def is_env_var_set(key: str, default: str) -> bool:
+    return str(os.environ.get(key, default)).lower() in ["true", "1"]
